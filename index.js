810f22ef9fdbe0557e5907c5e3029f350d07a5dd
Pradyu Vasudev <pvasu009@uottawa.ca>
2022-01-02 17:41:54 -0500

    maybe it works?

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ function loadTable(){
                 // Setting the sprite for the pokemon
                 //let imageSrc = pokemonDict[this.id][5].slice(1, pokemonDict[this.id][5].length - 1);
                 document.getElementById("spriteImage").src = "Images/" + this.id + ".png";
-
+                spriteImage.style.backgroundColor="rgba(255,255,255,0.5)";
                 // Setting the description for the pokemon
                 document.getElementById("description").innerHTML = pokemonDict[this.id][6];
 
